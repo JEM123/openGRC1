5f632d958192dea086c4e6c8a69e365f57030036
Lee Mangold <lmangold@outlook.com>
2025-09-26 17:32:45 -0400

    Handle 419 errors more gracefully

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Session\TokenMismatchException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -26,5 +27,18 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    /**
+     * Redirect to login page on TokenMismatchException.
+     */
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof TokenMismatchException) {
+            // Redirect to login page with a message
+            return redirect()->route('login')->with('error', 'Your session has expired.');
+        }
+
+        return parent::render($request, $exception);
     }
 }
